e07a0bf5ec7c3554df49ba6aa46dff1c38b74831
shreykoradia <shrey.koradia@yahoo.com>
2023-05-06 12:56:27 +0530

    added the code inside the repo

---
+++ constants/index.js
@@ -0,0 +1,52 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/planet-01.png',
+    title: 'The Hogwarts',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/planet-02.png',
+    title: 'The Upside Down',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/planet-03.png',
+    title: 'Edens Zero',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-04.png',
+    title: 'Paradise Island',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-05.png',
+    title: 'Hawkins Labs',
+  },
+];
+
+export const startingFeatures = [
+  'Find an Anime that suits you and you want to see around',
+  'Create Anime Content and Binge it on the Void Applications',
+  'No need to beat around the bush, just stay on the app and have fun',
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+  },
+];
